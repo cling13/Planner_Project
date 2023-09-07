18841bcd03c632ced676f8601dcd0e5e8a65e431
kimhanbeen <cling157@gmail.com>
2023-09-07 09:48:45 +0900

    리사이클러 리스트 완성

--- app/src/main/java/com/example/plannerproject010/MainActivity.java
+++ app/src/main/java/com/example/plannerproject010/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.plannerproject010;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -19,6 +20,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<String> list=new ArrayList<>();
+        for(int i=0; i<10; i++)
+        {
+            list.add(String.format("Text %d",i));
+        }
+
+        RecyclerView recyclerView=findViewById(R.id.planList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        SimpleAdapter adapter=new SimpleAdapter(list);
+        recyclerView.setAdapter(adapter);
     }
 }
 
