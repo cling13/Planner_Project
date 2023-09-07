977a6afaafd63ab655f6c0473f91790fa0bd31b7
kimhanbeen <cling157@gmail.com>
2023-09-07 10:24:09 +0900

    리사이클러뷰 데이터 추가 구현 완료

--- app/src/main/java/com/example/plannerproject010/MainActivity.java
+++ app/src/main/java/com/example/plannerproject010/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.plannerproject010;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -32,6 +37,25 @@ public class MainActivity extends AppCompatActivity {
 
         SimpleAdapter adapter=new SimpleAdapter(list);
         recyclerView.setAdapter(adapter);
+
+        Button btn=(Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dlg=new AlertDialog.Builder(MainActivity.this);
+                View v=(View) View.inflate(MainActivity.this,R.layout.plantextbox,null);
+                dlg.setView(v);
+                dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        EditText et=(EditText) v.findViewById(R.id.placetext);
+                        list.add(et.getText().toString());
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+                dlg.show();
+            }
+        });
     }
 }
 
